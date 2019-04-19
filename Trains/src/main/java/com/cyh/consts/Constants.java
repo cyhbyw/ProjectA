7bb6c2189466df58651fdb59ba721667c1693a39
cyhbyw <swust2009cyh@qq.com>
2019-04-19 08:57:33 +0800

    Trains

---
+++ Trains/src/main/java/com/cyh/consts/Constants.java
@@ -0,0 +1,29 @@
+package com.cyh.consts;
+
+/**
+ * @author: CYH
+ * @date: 2019/4/19 0019 8:33
+ */
+public interface Constants {
+
+    /**
+     * 输入文件名称
+     */
+    String INPUT_FILE_NAME = "input.txt";
+
+    /**
+     * 图描述的分隔符
+     */
+    String GRAPH_DESCRIPTION_SEPARATOR = ", ";
+
+    /**
+     * 最大的顶点数量
+     */
+    Integer MAX_POINT_COUNT = 26;
+
+    /**
+     * 最大的边数量
+     */
+    Integer MAX_EDGE_COUNT = 26 * 26;
+
+}
