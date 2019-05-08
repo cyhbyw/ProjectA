c33f830fefa2d1b2cb7867292ba7b29754b88103
cyhbyw <swust2009cyh@qq.com>
2019-05-08 08:28:18 +0800

    KMP

---
+++ algorithmAndDataStructure/src/main/java/com/cyh/str/match/KMP.java
@@ -0,0 +1,25 @@
+package com.cyh.str.match;
+
+/**
+ * @author: CYH
+ * @date: 2019/5/8 0008 8:24
+ */
+public class KMP {
+
+    public static int[] getNext(String ps) {
+        char[] p = ps.toCharArray();
+        int[] next = new int[p.length];
+        next[0] = -1;
+        int j = 0;
+        int k = -1;
+        while (j < p.length - 1) {
+            if (k == -1 || p[j] == p[k]) {
+                next[++j] = ++k;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
+}
---
+++ algorithmAndDataStructure/src/test/java/com/cyh/str/match/KMPTest.java
@@ -0,0 +1,18 @@
+package com.cyh.str.match;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * @author: CYH
+ * @date: 2019/5/8 0008 8:25
+ */
+public class KMPTest {
+
+    @Test
+    public void test_getNext() {
+        int[] next = KMP.getNext("abcabb");
+        System.out.println(Arrays.toString(next));
+    }
+}
