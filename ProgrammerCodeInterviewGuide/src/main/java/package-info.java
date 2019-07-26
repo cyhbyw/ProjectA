406a3a41a197489b09934c2972884fdc5b2fca80
cyhbyw <swust2009cyh@qq.com>
2019-07-26 08:57:05 +0800

    package-info

---
+++ ProgrammerCodeInterviewGuide/src/main/java/package-info.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * Created by CYH, 2019-07-26 08:18:33
+ *
+ */
+
+
+/******************************** 第1章：栈和队列 *****************************
+ * {@link chapter_1_stackandqueue.Problem_01_GetMinStack}
+ *    带有 getMin() 功能的栈
+ * {@link chapter_1_stackandqueue.Problem_02_TwoStacksImplementQueue}
+ *    由两个栈组成的队列
+ * {@link chapter_1_stackandqueue.Problem_03_ReverseStackUsingRecursive}
+ *    仅用递归函数和桡操作逆序一个栈
+ *    有意思，可以多写写 # TODO
+ * {@link chapter_1_stackandqueue.Problem_04_DogCatQueue}
+ *    猫狗队列
+ *    数据结构的设计比较巧妙 # TODO
+ * {@link chapter_1_stackandqueue.Problem_05_StackSortStack}
+ *    用一个栈实现另一个栈的排序
+ * {@link chapter_1_stackandqueue.Problem_06_HanoiStack}
+ *    用栈来解决汉诺塔问题
+ * {@link chapter_1_stackandqueue.Problem_07_SlidingWindowMaxArray}
+ *    生成窗口最大值数组 # TODO coding
+ * {@link chapter_1_stackandqueue.Problem_08_MonotonousStack}
+ *    单调栈结构 # TODO 难题，还未想通
+ * {@link chapter_1_stackandqueue.Problem_09_MaximalRectangle}
+ *    求最大子矩阵的大小 # TODO 难题，还未想通
+ * {@link chapter_1_stackandqueue.Problem_10_AllLessNumSubArray}
+ *    最大值减去最小值小于等于num的子数组数量 # TODO 难题，还未想通
+ * {@link chapter_1_stackandqueue.Problem_11_VisibleMountains}
+ *    可见的山峰对数量 # TODO 难题，还未想通
+ *******************************************************************************/
+
+/******************************** 第2章：链表问题 *****************************
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ *    打印两个有序链表的公共部分
+ * {@link chapter_2_listproblem.Problem_02_RemoveLastKthNode}
+ *    在单链表和双链表中删除倒数第K个节点
+ * {@link chapter_2_listproblem.Problem_03_RemoveNodeByRatio}
+ *    删除链表的中间节点和a/b处的节点
+ * {@link chapter_2_listproblem.Problem_04_ReverseList}
+ *    反转单向和双向链表
+ *    两个变量即可搞定 # TODO coding
+ * {@link chapter_2_listproblem.Problem_05_ReversePartList}
+ *    反转部分单向链表
+ * {@link chapter_2_listproblem.Problem_06_JosephusProblem}
+ *    环形单链表的约瑟夫问题
+ * {@link chapter_2_listproblem.Problem_07_IsPalindromeList}
+ *    判断一个链表是否为回文结构
+ *    方法三中没有使用辅助空间，只用几个变量变能实现且能够还原原来的链表，可以看看 # TODO
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ * {@link chapter_2_listproblem.Problem_01_PrintCommonPart}
+ *******************************************************************************/
