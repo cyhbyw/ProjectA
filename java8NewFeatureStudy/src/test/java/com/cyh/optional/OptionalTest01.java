890198057ffdb9eb219d2306e164b18a9f0ce296
yanhua.chen <yanhua.chen@faw-vw.com>
2021-01-22 16:01:49 +0800

    Optional.ofNullable().map

--- java8NewFeatureStudy/src/test/java/com/cyh/optional/OptionalTest01.java
+++ java8NewFeatureStudy/src/test/java/com/cyh/optional/OptionalTest01.java
@@ -3,8 +3,8 @@ package com.cyh.optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.cyh.pojo.Person;
 import java.util.Optional;
-
 import org.junit.Test;
 
 /**
@@ -21,6 +21,16 @@ public class OptionalTest01 {
         optional.ifPresent((s) -> System.out.println(s.charAt(0))); // "b"
     }
 
+    @Test
+    public void test02() {
+        Person person = null;
+        String firstName = Optional.ofNullable(person).map(x -> x.getFirstName()).orElse("null");
+        System.out.println("one: " + firstName);
+
+        person = new Person("yanhua", "chen");
+        firstName = Optional.ofNullable(person).map(x -> x.getFirstName()).orElse("null");
+        System.out.println("two: " + firstName);
+    }
 
 
 }
