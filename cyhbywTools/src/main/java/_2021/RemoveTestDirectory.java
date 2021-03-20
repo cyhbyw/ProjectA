cbe1c175253436618ab1dcd4ebb34b4807bb7a71
cyhbyw <swust2009cyh@qq.com>
2021-03-20 08:55:35 +0800

    删除开源 Java 项目下的 src/test 目录

--- cyhbywTools/src/main/java/_2021/RemoveTestDirectory.java
+++ cyhbywTools/src/main/java/_2021/RemoveTestDirectory.java
@@ -1,12 +1,12 @@
 package _2021;
 
+import org.apache.commons.io.FileUtils;
+
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
-
-import org.apache.commons.io.FileUtils;
 
 /**
  * 删除开源 Java 项目下的 src/test 目录
@@ -16,20 +16,21 @@ import org.apache.commons.io.FileUtils;
  */
 public class RemoveTestDirectory {
 
-    public static void main(String[] args) throws Exception {
-        Path rootDir = Paths.get("D:\\openSource\\spring-framework");
-        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(rootDir);
-        Iterator<Path> iterator = directoryStream.iterator();
-        while (iterator.hasNext()) {
-            Path next = iterator.next();
-            if (!Files.isDirectory(next)) {
-                continue;
-            }
-            Path test = next.resolve("src/test");
-            if (Files.exists(test)) {
-                FileUtils.deleteDirectory(test.toFile());
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		Path rootDir = Paths.get("D:\\open-sources\\Nacos");
+		DirectoryStream<Path> directoryStream = Files.newDirectoryStream(rootDir);
+		Iterator<Path> iterator = directoryStream.iterator();
+		while (iterator.hasNext()) {
+			Path next = iterator.next();
+			if (!Files.isDirectory(next)) {
+				continue;
+			}
+			Path test = next.resolve("src/test");
+			if (Files.exists(test)) {
+				FileUtils.deleteDirectory(test.toFile());
+				System.out.println("Delete dir: " + test);
+			}
+		}
+	}
 
 }
